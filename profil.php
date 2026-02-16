c2ffe368b8c28e5c6353b0962fe529b989d4dc75
wissal ghzaiel <wyssal.ghzaiel@gmail.com>
2026-02-16 09:16:57 +0100

    mon premier projet

---
+++ profil.php
@@ -0,0 +1,18 @@
+<?php
+$nom = "AYARI";
+$prenom = "Asma";
+$email = "asma.ayari@email.com";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Profil utilisateur</title>
+</head>
+<body>
+<h1>Profil utilisateur</h1>
+<p><strong>Nom :</strong> <?= $nom ?></p>
+<p><strong>Prénom :</strong> <?= $prenom ?></p>
+<p><strong>Email :</strong> <?= $email ?></p>
+</body>
+</html>
