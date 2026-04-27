29e406bd5cd03ff21ac0ac0777b16b48edc88e49
wissal ghzaiel <wyssal.ghzaiel@gmail.com>
2026-04-27 15:41:50 +0100

    tp4

---
+++ includes/fonctions.php
@@ -0,0 +1,16 @@
+<?php
+function afficherErreur($erreur) {
+if (!empty($erreur)) {
+return "<div style='color:red;'>$erreur</div>";
+}
+}
+function afficherSucces($nom, $prenom, $email) {
+return "<div style='color:green;'>
+Formulaire valide ✔<br>
+Nom : $nom <br>
+Prénom : $prenom <br>
+Email : $email
+</div>";
+
+}
+?>
--- traitement.php
+++ traitement.php
@@ -1,42 +1,16 @@
-<?php
-
-$erreur = "";
-  // Récupération des données
-    $nom = $_POST["nom"];
-    $prenom = $_POST["prenom"];
-    $email = $_POST["email"];
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Vérifications
-
-    if (empty($nom)) {
-        $erreur .= "Le nom est obligatoire.<br>";
-    }
-
-    if (empty($prenom)) {
-        $erreur .= "Le prénom est obligatoire.<br>";
-    }
-
-    if (empty($email)) {
-        $erreur .= "L'email est obligatoire.<br>";
-    }
-
-    if (!empty($email) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $erreur .= "Format email invalide.<br>";
-    }
-
-    // Affichage des erreurs ou succès
-
-    if (!empty($erreur)) {
-        echo "<div style='color:red;'>$erreur</div>";
-    } else {
-        echo "<div style='color:green;'>Formulaire validé </div>";
-        echo "Nom : $nom <br>";
-        echo "Prénom : $prenom <br>";
-        echo "Email : $email";
-    }
-
-} else {
-    echo "<div style='color:red;'>Accès invalide au fichier.</div>";
-}
-
+<?php 
+require "includes/fonctions.php"; 
+require "includes/validation.php"; 
+if ($_SERVER["REQUEST_METHOD"] == "POST") { 
+$nom = trim($_POST["nom"]); 
+$prenom = trim($_POST["prenom"]); 
+$email = trim($_POST["email"]); 
+// Validation 
+$erreur = validerFormulaire($nom, $prenom, $email); 
+// Affichage 
+if (!empty($erreur)) { 
+echo afficherErreur($erreur); 
+} else { 
+echo afficherSucces($nom, $prenom, $email); } 
+} 
+?> 
